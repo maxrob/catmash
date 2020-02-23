0621b749f8bb86d28bccb43eb3ee16317abe515c
Maxime Robin <maxime@liberkeys.com>
2020-02-23 20:48:28 +0100

    Add dotenv for env variable

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv'
 
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World !');
 });
