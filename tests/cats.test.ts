bbd82212c2f25adeb5c16d0c2558c05210884a1e
Maxime Robin <maxime@liberkeys.com>
2020-02-25 00:04:02 +0100

    Add tests for add cat point endpoint

--- tests/cats.test.ts
+++ tests/cats.test.ts
@@ -2,6 +2,7 @@ import supertest from 'supertest';
 import app from '../src/app';
 import dotenv from 'dotenv';
 import { connectDB, disconnectDB } from '../src/lib/mongoose';
+import { Cat } from '../src/models/cat';
 
 const request = supertest(app);
 
@@ -11,7 +12,7 @@ beforeAll(connectDB);
 
 afterAll(disconnectDB);
 
-describe('Get Cats Mashes', () => {
+describe('Get Cats', () => {
   it('should return a 200 response', async () => {
     const res = await request.get('/cats');
     expect(res.statusCode).toEqual(200);
@@ -62,5 +63,18 @@ describe('Get Cats Mashes', () => {
   it('should return array of cats with attribute score', async () => {
     const res = await request.get('/cats/mashes');
     expect(res.body[0][0]).toHaveProperty('score');
+  });
+});
+
+describe('Add Cat Point', () => {
+  it('should return a 200 response', async () => {
+    const cat: any = await Cat.findOne();
+    const res = await request.post(`/cats/${cat._id}/add_point`);
+    expect(res.statusCode).toEqual(200);
+  });
+  it('should return cat with increment score', async () => {
+    const cat: any = await Cat.findOne();
+    const res = await request.post(`/cats/${cat._id}/add_point`);
+    expect(res.body.score).toEqual(cat.score + 1);
   });
 });
