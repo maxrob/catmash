b734a9cfdb23970a7d895f5a46f272c31dc41b23
Maxime Robin <maxime@liberkeys.com>
2020-04-20 18:40:45 +0200

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors'
 import { getCats, getCatsMashes, addCatPoint } from './controllers/cats';
 
 const app = express();
+app.use(cors())
 app.get('/cats', getCats);
 app.get('/cats/mashes', getCatsMashes);
 app.post('/cats/:id/add_point', addCatPoint);
